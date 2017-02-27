683f70b346565276c90cc57c75bff303e88d8d9a
Jcook894 <jcook894@icloud.com>
2017-02-27 13:42:07 -0500

    Fix: Deleted the test routes in routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,11 @@ import { Route, IndexRoute  } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 
-const Greeting = () => {
-  return <div>YO what it do?</div>;
-};
+
 
 export default(
   <Route path="/" component={App} >
     <IndexRoute component={PostsIndex} />
-    <Route path="greet" component={Greeting}/>
-    <Route path="greet2" component={Greeting}/>
-    <Route path="greet3" component={Greeting}/>
   </Route>
 );
 // google.com/ => render App.
