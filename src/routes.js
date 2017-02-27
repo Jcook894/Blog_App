71bfc378a408c195c790fee70e2392c6becffd6e
Jcook894 <jcook894@icloud.com>
2017-02-27 13:40:39 -0500

    Feat: Added post_index component and rendered it in the route index.

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, IndexRoute  } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/posts_index';
 
 const Greeting = () => {
   return <div>YO what it do?</div>;
@@ -9,6 +10,7 @@ const Greeting = () => {
 
 export default(
   <Route path="/" component={App} >
+    <IndexRoute component={PostsIndex} />
     <Route path="greet" component={Greeting}/>
     <Route path="greet2" component={Greeting}/>
     <Route path="greet3" component={Greeting}/>
