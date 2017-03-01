6c6f585248528d61d70b47cd74ae68d93095c723
Jcook894 <jcook894@icloud.com>
2017-03-01 15:39:41 -0500

    Feat: Wired up the config obj to the input to get access to redux form.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -10,17 +10,17 @@ class PostsNew extends Component {
         <h3>Create a New Post</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title}/>
         </div>
 
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...category}/>
         </div>
 
         <div className="form-group">
           <label>Content</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content}/>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
