5ad0174cdf1a9f6031bae271c576c32d7f57a798
Jcook894 <jcook894@icloud.com>
2017-03-01 14:35:53 -0500

    Feat: Set up the form content in post_new comp.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,7 +4,25 @@ import { reduxForm } from 'redux-form';
 class PostsNew extends Component {
   render() {
     return(
-      <div>Create Form</div>
+      <form>
+        <h3>Create a New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
@@ -13,3 +31,5 @@ export default reduxForm({
   form: 'PostNew',
   fields:['title', 'category', 'content']
 })(PostsNew);
+
+/// Whenever a user types something in.... record it on app state.
