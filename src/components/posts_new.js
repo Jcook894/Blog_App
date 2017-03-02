d4f142dffb5419ea10d6712e6238515236c2828b
Jcook894 <jcook894@icloud.com>
2017-03-02 02:43:07 -0500

    Feat: Added more validation to the form inputs. Also added a cancel btn that takes you back to route dir.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link }  from 'react-router';
+
 
 class PostsNew extends Component {
   render() {
@@ -9,7 +11,7 @@ class PostsNew extends Component {
     return(
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create a New Post</h3>
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? "has-danger" : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title}/>
           <div className="text-help">
@@ -17,7 +19,7 @@ class PostsNew extends Component {
           </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${category.touched && category.invalid ? "has-danger" : ''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...category}/>
           <div className="text-help">
@@ -25,7 +27,7 @@ class PostsNew extends Component {
           </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? "has-danger" : ''}`}>
           <label>Content</label>
           <textarea className="form-control" {...content}/>
           <div className="text-help">
@@ -34,6 +36,9 @@ class PostsNew extends Component {
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">
+          Cancel
+        </Link>
       </form>
     );
   }
