78f4933cde1cba0854ffa57636bf3ac44a980e58
Jcook894 <jcook894@icloud.com>
2017-03-11 14:17:54 -0500

    Feat: added a new route to the post show comp.

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ export default(
   <Route path="/" component={App} >
     <IndexRoute component={PostsIndex} />
     <Route path="posts/new" component={PostsNew} />
+    <Route path="posts/:id" component={PostsShow} />
   </Route>
 );
 // google.com/ => render App.
