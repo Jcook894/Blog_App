b82ea6d33c841be984fe21a040cdc93f969c35bf
Jcook894 <jcook894@icloud.com>
2017-03-14 22:16:14 -0400

    Feat: When user deletes single post, send user back to index page.

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,27 @@
-import React, { Component } from "react" ;
+import React, { Component, PropTypes } from "react" ;
 import { connect }  from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
 import { Link } from 'react-router';
 
 
 class PostsShow extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
   componentWillMount(){
     this.props.fetchPost(this.props.params.id);
   }
+
+  onDeleteClick(){
+    this.props.deletePost(this.props.params.id)
+    .then(() =>{
+        this.context.router.push('/');
+      });
+
+  }
+
+
   render() {
     const { post } = this.props;
     if(!post){
@@ -18,6 +32,10 @@ class PostsShow extends Component {
 
         <div>
           <Link to="/">Back to Index</Link>
+          <button className="btn btn-danger pull-xs-right"
+            onClick={this.onDeleteClick.bind(this)}>
+            Delete Post
+          </button>
           <h3>{post.title}</h3>
           <h6>{post.category}</h6>
           <p>{post.content}</p>
@@ -31,4 +49,4 @@ function mapStateToProps(state) {
   return { post: state.post.post };
 }
 
-export default connect(mapStateToProps,{ fetchPost })(PostsShow);
+export default connect(mapStateToProps,{ fetchPost, deletePost })(PostsShow);
